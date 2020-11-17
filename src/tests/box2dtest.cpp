6389084d1edcb629761e809641548aa86aca14c7
Paolo <paolo.oliverio.dev@gmail.com>
2020-11-17 14:52:44 +0100

    box2d sample scale 1px = 1cm

--- src/tests/box2dtest.cpp
+++ src/tests/box2dtest.cpp
@@ -7,9 +7,10 @@
 
 
 using namespace kiss;
+constexpr float physcale = 100.f;
 
 namespace {
-	b2World world = b2World({ 0.f, 720.f });
+	b2World world = b2World({ 0.f, 9.81 });
 	float	elapsed = 0;
 	b2Body* groundBody;
 	b2Body* body;
@@ -19,36 +20,36 @@ namespace {
 namespace box2dtest {
 	void init() {
 		b2BodyDef groundBodyDef;
-		groundBodyDef.position.Set(640.f, 500.0f);
+		groundBodyDef.position.Set(6.40f, 5.f);
 		groundBodyDef.angle = deg2rad(20);
 		groundBody = world.CreateBody(&groundBodyDef);
 		b2PolygonShape groundBox;
-		groundBox.SetAsBox(1000.0f, 5.0f);
+		groundBox.SetAsBox(10.0f * physcale, 0.05f);
 		groundBody->CreateFixture(&groundBox, 0.0f);
 
 		b2PolygonShape dynamicBox;
 
 		b2Vec2 vertices[4];
-		vertices[0].Set(-16.f, 0.f);
-		vertices[1].Set(10.f, 0.f);
-		vertices[2].Set(10.f, -32.f);
-		vertices[3].Set(-16.f, -32.f);
+		vertices[0].Set(-0.16f, 0.f);
+		vertices[1].Set(0.1f, 0.f);
+		vertices[2].Set(0.1f, -0.32f);
+		vertices[3].Set(-0.16f, -0.32f);
 
 		dynamicBox.Set(vertices, 4);
 
 		b2FixtureDef fixtureDef;
 		fixtureDef.shape = &dynamicBox;
 		fixtureDef.density = 1.0f;
-		fixtureDef.friction = 0.5f;
-		fixtureDef.restitution = 0.8f;
+		fixtureDef.friction = 0.75f;
+		fixtureDef.restitution = 0.75f;
 		b2BodyDef bodyDef;
 		bodyDef.type = b2_dynamicBody;
 
-		bodyDef.position.Set(100.0f, 100.0f);
+		bodyDef.position.Set(1.0f , 1.0f);
 		body = world.CreateBody(&bodyDef);
 		body->CreateFixture(&fixtureDef);
 
-		bodyDef.position.Set(90.0f, 132.0f);
+		bodyDef.position.Set(0.9f , 1.3f);
 		body2 = world.CreateBody(&bodyDef);
 		body2->CreateFixture(&fixtureDef);
 	}
@@ -58,15 +59,15 @@ namespace box2dtest {
 		elapsed += dt;
 
 		if (elapsed > 10) {
-			body->SetTransform(b2Vec2(100 + (float)kinc_random_get_in(-10, 10), 100), 0);
+			body->SetTransform(b2Vec2(1.f + (float)kinc_random_get_in(-10, 10) / physcale, 1.0f), 0);
 			body->SetAngularVelocity(0);
 			body->SetLinearVelocity(b2Vec2(0, 0));
-			body2->SetTransform(b2Vec2(100 + (float)kinc_random_get_in(-20, 20), 132), 0);
+			body2->SetTransform(b2Vec2(1.f + (float)kinc_random_get_in(-20, 20) / physcale, 1.32f), 0);
 			body2->SetAngularVelocity(0);
 			body2->SetLinearVelocity(b2Vec2(0, 0));
 			elapsed = 0;
 		}
-		world.Step(dt, 10, 10);
+		world.Step(dt, 6, 2);
 	}
 
 	void render()
@@ -74,9 +75,9 @@ namespace box2dtest {
 		auto a = body->GetTransform();
 		auto quads = gfx2d::quad::batcher;
 		quads->sprite(id::spr::RectAnim1, 100, 100, rot(deg2rad(elapsed * 360)), iColor::White);
-		quads->sprite(id::spr::RectAnim1, a.p.x, a.p.y, rot(a.q.c, a.q.s), iColor::White);
+		quads->sprite(id::spr::RectAnim1, a.p.x * physcale, a.p.y * physcale, rot(a.q.c, a.q.s), iColor::White);
 		a = body2->GetTransform();
-		quads->sprite(id::spr::RectAnim1, a.p.x, a.p.y, rot(a.q.c, a.q.s), iColor::White);
+		quads->sprite(id::spr::RectAnim1, a.p.x * physcale, a.p.y * physcale, rot(a.q.c, a.q.s), iColor::White);
 	}
 }
 #endif
